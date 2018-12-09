78b36d3dffc4bafa2a02481c725b10fa91e73542
Stepan Borysenko <Stepan1983@gmail.com>
2018-12-09 20:25:25 +0200

    added ship unloading2

--- src/main/java/com/gmail/stepan1983/probe/Probe.java
+++ src/main/java/com/gmail/stepan1983/probe/Probe.java
@@ -1,4 +1,16 @@
 package com.gmail.stepan1983.probe;
 
 public class Probe {
+
+    public static void main(String[] str) {
+        outer:
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println("Hello");
+                continue outer;
+            }
+            System.out.println("outer");
+        }
+        System.out.println("Good-Bye");
+    }
 }
--- src/main/java/com/gmail/stepan1983/shipUnloading/Ship.java
+++ src/main/java/com/gmail/stepan1983/shipUnloading/Ship.java
@@ -1,4 +1,6 @@
 package com.gmail.stepan1983.shipUnloading;
 
 public class Ship {
+
+
 }
